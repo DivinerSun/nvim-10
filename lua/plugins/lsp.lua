fdaa50b79ad30a485d658187b1030e1badfd92ac
Diviner <sqq_job@sina.com>
2024-12-17 10:44:20 +0800

    feat: 自定义快捷键

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+
+  -- LSP 信息多行显示
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      vim.diagnostic.config({
+        virtual_text = false,
+      })
+      require("lsp_lines").setup()
+    end,
+  },
+}
