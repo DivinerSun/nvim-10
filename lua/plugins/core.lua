e4bcee982ec886f1720cd1b51845e6e52d82e38d
Diviner <sqq_job@sina.com>
2024-12-17 10:36:45 +0800

    feat: 配置主题颜色

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -12,7 +12,46 @@ return {
       styles = {
         sidebars = "transparent",
         floats = "transparent",
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = { italic = true },
       },
+      on_colors = function(C)
+        C.comment = "#FF81D0"
+        C.fg_gutter = "#813c85"
+
+        return {
+          Comment = { fg = C.pink },
+          CmpItemMenu = { fg = C.pink, bg = C.None },
+          CmpItemKindSnippet = { fg = C.base, bg = C.mauve },
+          CmpItemKindKeyword = { fg = C.base, bg = C.red },
+          CmpItemKindText = { fg = C.base, bg = C.lavender },
+          CmpItemKindMethod = { fg = C.base, bg = C.blue },
+          CmpItemKindConstructor = { fg = C.base, bg = C.blue },
+          CmpItemKindFunction = { fg = C.base, bg = C.blue },
+          CmpItemKindFolder = { fg = C.base, bg = C.blue },
+          CmpItemKindModule = { fg = C.base, bg = C.blue },
+          CmpItemKindConstant = { fg = C.base, bg = C.peach },
+          CmpItemKindField = { fg = C.base, bg = C.green },
+          CmpItemKindProperty = { fg = C.base, bg = C.green },
+          CmpItemKindEnum = { fg = C.base, bg = C.green },
+          CmpItemKindUnit = { fg = C.base, bg = C.green },
+          CmpItemKindClass = { fg = C.base, bg = C.yellow },
+          CmpItemKindVariable = { fg = C.base, bg = C.flamingo },
+          CmpItemKindFile = { fg = C.base, bg = C.blue },
+          CmpItemKindInterface = { fg = C.base, bg = C.yellow },
+          CmpItemKindColor = { fg = C.base, bg = C.red },
+          CmpItemKindReference = { fg = C.base, bg = C.red },
+          CmpItemKindEnumMember = { fg = C.base, bg = C.red },
+          CmpItemKindStruct = { fg = C.base, bg = C.blue },
+          CmpItemKindValue = { fg = C.base, bg = C.peach },
+          CmpItemKindEvent = { fg = C.base, bg = C.blue },
+          CmpItemKindOperator = { fg = C.base, bg = C.blue },
+          CmpItemKindTypeParameter = { fg = C.base, bg = C.blue },
+          CmpItemKindCopilot = { fg = C.base, bg = C.teal },
+          CmpItemKindCodeium = { fg = C.base, bg = C.teal },
+        }
+      end,
     },
   },
 }
