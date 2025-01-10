bee01c27ed5ad675a84417b12c3a2a02aa9480d1
Diviner <sqq_job@sina.com>
2025-01-10 10:42:29 +0800

    feat: 默认不显示多行错误信息

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
     config = function()
       vim.diagnostic.config({
         virtual_text = false,
+        virtual_lines = false,
       })
       require("lsp_lines").setup()
     end,
