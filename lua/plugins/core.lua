98a1b6b0637ae76a50f2517f60648b12d306e894
Diviner <sqq_job@sina.com>
2025-10-10 10:22:46 +0800

    feat: 配置catppuccin主题

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "catppuccin",
     },
   },
   -- 自定义 Which-Key 布局格式
@@ -25,6 +25,214 @@ return {
         },
       },
     },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false, -- 确保插件立即加载
+    priority = 1000, -- 优先加载主题
+    opts = {
+      flavour = "auto", -- latte, frappe, macchiato, mocha
+      transparent_background = true, -- 启用透明背景
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+      float = {
+        transparent = true, -- enable transparent floating windows
+        solid = true, -- use solid styling for floating windows, see |winborder|
+      },
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      no_underline = false, -- Force no underline
+      styles = {
+        comments = { "italic", "bold" },
+        properties = { "bold" },
+        functions = { "bold" },
+        keywords = { "italic", "bold" },
+        operators = { "bold" },
+        conditionals = { "italic" },
+        loops = { "italic" },
+        booleans = { "bold", "italic" },
+        numbers = {},
+        types = {},
+        strings = {},
+        variables = {},
+      },
+      lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+          ok = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+          ok = { "underline" },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+      color_overrides = {
+        all = {
+          -- text = "#acb8f4",
+        },
+      },
+      custom_highlights = function(colors)
+        return {
+          Comment = { fg = "#FF81D0" },
+          Gutter = { fg = "#813c85" },
+          TabLineSel = { bg = colors.pink },
+          CmpBorder = { fg = colors.surface2 },
+          Pmenu = { bg = colors.none },
+        }
+      end,
+      highlight_overrides = {
+        all = function(cp)
+          local clear = {}
+          local transparent_background = true
+          return {
+            -- For base configs
+            NormalFloat = { fg = cp.text, bg = transparent_background and cp.none or cp.mantle },
+            FloatBorder = {
+              fg = transparent_background and cp.blue or cp.mantle,
+              bg = transparent_background and cp.none or cp.mantle,
+            },
+            CursorLineNr = { fg = cp.green },
+
+            -- For native lsp configs
+            DiagnosticVirtualTextError = { bg = cp.none },
+            DiagnosticVirtualTextWarn = { bg = cp.none },
+            DiagnosticVirtualTextInfo = { bg = cp.none },
+            DiagnosticVirtualTextHint = { bg = cp.none },
+            LspInfoBorder = { link = "FloatBorder" },
+
+            -- For mason.nvim
+            MasonNormal = { link = "NormalFloat" },
+
+            -- For indent-blankline
+            IblIndent = { fg = cp.surface0 },
+            IblScope = { fg = cp.surface2, style = { "bold" } },
+
+            -- For nvim-cmp and wilder.nvim
+            Pmenu = { fg = cp.overlay2, bg = transparent_background and cp.none or cp.base },
+            PmenuBorder = { fg = cp.surface1, bg = transparent_background and cp.none or cp.base },
+            PmenuSel = { bg = cp.green, fg = cp.base },
+            CmpItemAbbr = { fg = cp.overlay2 },
+            CmpItemAbbrMatch = { fg = cp.blue, style = { "bold" } },
+            CmpDoc = { link = "NormalFloat" },
+            CmpDocBorder = {
+              fg = transparent_background and cp.surface1 or cp.mantle,
+              bg = transparent_background and cp.none or cp.mantle,
+            },
+
+            -- For fidget
+            FidgetTask = { bg = cp.none, fg = cp.surface2 },
+            FidgetTitle = { fg = cp.blue, style = { "bold" } },
+
+            -- For nvim-notify
+            NotifyBackground = { bg = cp.base },
+
+            -- For nvim-tree
+            NvimTreeRootFolder = { fg = cp.pink },
+            NvimTreeIndentMarker = { fg = cp.surface2 },
+
+            -- For trouble.nvim
+            TroubleNormal = { bg = transparent_background and cp.none or cp.base },
+            TroubleNormalNC = { bg = transparent_background and cp.none or cp.base },
+
+            -- For telescope.nvim
+            TelescopeMatching = { fg = cp.lavender },
+            TelescopeResultsDiffAdd = { fg = cp.green },
+            TelescopeResultsDiffChange = { fg = cp.yellow },
+            TelescopeResultsDiffDelete = { fg = cp.red },
+
+            -- For glance.nvim
+            GlanceWinBarFilename = { fg = cp.subtext1, style = { "bold" } },
+            GlanceWinBarFilepath = { fg = cp.subtext0, style = { "italic" } },
+            GlanceWinBarTitle = { fg = cp.teal, style = { "bold" } },
+            GlanceListCount = { fg = cp.lavender },
+            GlanceListFilepath = { link = "Comment" },
+            GlanceListFilename = { fg = cp.blue },
+            GlanceListMatch = { fg = cp.lavender, style = { "bold" } },
+            GlanceFoldIcon = { fg = cp.green },
+
+            -- For nvim-treehopper
+            TSNodeKey = {
+              fg = cp.peach,
+              bg = transparent_background and cp.none or cp.base,
+              style = { "bold", "underline" },
+            },
+
+            -- For treesitter
+            ["@keyword.return"] = { fg = cp.pink, style = clear },
+            ["@error.c"] = { fg = cp.none, style = clear },
+            ["@error.cpp"] = { fg = cp.none, style = clear },
+          }
+        end,
+      },
+      default_integrations = true,
+      auto_integrations = false,
+      integrations = {
+        cmp = true,
+        dap = true,
+        dap_ui = true,
+        diffview = true,
+        dropbar = { enabled = true, color_mode = true },
+        fidget = true,
+        flash = true,
+        fzf = true,
+        gitsigns = true,
+        grug_far = true,
+        hop = true,
+        indent_blankline = { enabled = true, colored_indent_levels = true },
+        lsp_saga = true,
+        lsp_trouble = true,
+        markdown = true,
+        mason = true,
+        mini = { enabled = true },
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+        },
+        notify = true,
+        nvimtree = true,
+        rainbow_delimiters = true,
+        render_markdown = true,
+        semantic_tokens = true,
+        telescope = { enabled = true, style = "nvchad" },
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin") -- 应用主题
+    end,
   },
   -- 自定义Tokyonight主题配置
   {
