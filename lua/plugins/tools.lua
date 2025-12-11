c0c019e38b88df1dd3b5e7acb3387cdcbd6a781a
Diviner <sqq_job@sina.com>
2025-12-11 16:24:22 +0800

    feat: 工具配置 - 文件树NeoTree

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -1,34 +1,40 @@
 return {
-	-- 内部终端
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		opts = {
-			size = 20,
-			open_mapping = [[<C-t>]],
-			hide_numbers = true,
-			shade_filetypes = {},
-			shade_terminals = true,
-			shading_factor = 2,
-			start_in_insert = true,
-			insert_mappings = true,
-			persist_size = true,
-			direction = "float",
-			close_on_exit = true,
-			shell = vim.o.shell,
-			float_opts = {
-				border = "curved",
-				winblend = 0,
-				highlights = {
-					border = "Normal",
-					background = "Normal",
+-- 文件树插件
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
+    config = function()
+			vim.keymap.set("n", "<leader>e", "<Cmd>Neotree toggle reveal<CR>", { desc = "Toggle File Tree" })
+			require("neo-tree").setup({
+				enable_git_status = true,
+				enable_diagnostics = true,
+				sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+				open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" }, -- when opening files, do not use windows containing these filetypes or buftypes
+				window = {
+					position = "float",
+					width = 80,
+					mapping_options = {
+						noremap = true,
+						nowait = true,
+					},
+					mappings = {
+						["<space>"] = {
+							"toggle_node",
+							nowait = false,
+						},
+						["<2-LeftMouse>"] = "open",
+						["<cr>"] = "open",
+						["l"] = "open",
+						["<esc>"] = "cancel",
+					},
 				},
-			},
-		},
-	},
-	-- 多光标
-	{
-		"mg979/vim-visual-multi",
-		event = "BufWinEnter",
-	},
+			})
+		end,
+  }
 }
