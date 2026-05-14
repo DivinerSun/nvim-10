69be50a0d2b5dd41541c468dd13f69bfc1c4a052
Diviner <sqq_job@sina.com>
2026-05-14 22:44:47 +0800

    feat: neovim pack - custom which-key config

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -18,7 +18,7 @@ fzf.setup({
 
 vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Live grep" })
-vim.keymap.set("n", "<leader>/", fzf.live_grep, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>/", "<cmd>FzfLua live_grep<CR>", { desc = "Find text" })
 vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Find buffers" })
 vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "Help tags" })
 vim.keymap.set("n", "<leader>fr", fzf.oldfiles, { desc = "Recent files" })
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,26 @@
-require("which-key").setup()
+require("which-key").setup({
+	preset = "helix",
+	icons = { group = "" },
+	spec = {
+		{
+			mode = { "n", "x" },
+			-- { "<leader>a", group = "AICode", icon = " " },
+			-- { "<leader>b", group = " Buffer", icon = " " },
+			{ "<leader>c", group = " Codes", icon = " " },
+			{ "<leader>d", group = " Debug", icon = " " },
+			{ "<leader>f", group = " Find", icon = " " },
+			{ "<leader>g", group = " Git", icon = " " },
+			-- { "<leader>p", group = "Preview/Paste", icon = " " },
+			{ "<leader>q", group = " Session", icon = " " },
+			{ "<leader>s", group = " Split", icon = " " },
+			{ "<leader>t", group = " Test", icon = "󰰦 " },
+			-- { "<leader>u", group = " UI", icon = " " },
+		},
+	},
+})
+vim.keymap.set("n", "<leader>?", function()
+	require("which-key").show({ global = false })
+end, { desc = "Buffer Local KeyMaps" })
 
 require("lualine").setup({
 	options = {
